c6d5467dbadf66fa951f9d40ee68b57d45a3f34a
Ethan Safai <ethan.safai@gmail.com>
2024-02-05 19:29:21 -0800

    styling, add home page skeleton

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,13 @@
 import { Link } from 'react-router-dom'
+import { ClipboardIcon } from '@heroicons/react/outline'
 
 function Header() {
   return (
-    <header className="p-4 bg-white shadow-md mb-4 sticky top-0">
+    <header
+      className="p-4 bg-white shadow-md mb-4 sticky top-0 flex
+    justify-center items-center gap-2"
+    >
+      <ClipboardIcon className="h-6 w-6 mt-[2px]" />
       <Link
         to="/"
         className="no-underline text-2xl font-semibold text-center text-black 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,26 @@
+import { useContext } from 'react'
+import { UserContext } from '../context/AppContext'
+
 function Home() {
+  const { user, setUser } = useContext(UserContext)
+
+  function handleLogout() {
+    setUser(null)
+  }
+
   // TODO: implement login screen (this is placeholder code)
   return (
-    <div>
-      <h1 className="text-lg font-bold text-red-500">This is a home screen</h1>
+    <div className="flex flex-col gap-4 justify-center">
+      <h1 className="text-lg font-bold text-center">
+        Welcome, {user.firstName} {user.lastName}
+      </h1>
+      <button
+        type="button"
+        className="mx-auto w-32 block bg-white"
+        onClick={handleLogout}
+      >
+        Sign Out
+      </button>
     </div>
   )
 }
